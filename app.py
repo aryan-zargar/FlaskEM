79b8a1dc8d91217ced99c94e85816833bb1706df
aryanzargar1390 <aryan.zargar90@gmail.com>
2023-11-08 20:15:27 +0330

    Added test.html for Example

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 # @app.route("/")
 # def function():
 #   FlaskEm.AddPage("main.html",a=5,b=7)
+
+# @app.route("/test")
+# def function():
+#   FlaskEm.AddPage("test.html",abbas='484',b=7)

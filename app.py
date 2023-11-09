6c8eb7d1ec205dac10e632ec98ee788f10a9707f
aryanzargar1390 <aryan.zargar90@gmail.com>
2023-11-09 07:49:28 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,4 @@
-'''
-to make a database you need to do in cmd : 
-
-flask shell 
-- 
-from app import db
-db.create_all()
-
-# just like flask
-'''
 import commands.commands as FlaskEm
 from flask import Flask
 app = Flask(__name__)
 db = FlaskEm.ConfigDataBase(app,"sqlite:///YOUR_DATABASE_NAME.db")
-# @app.route("/")
-# def function():
-#   FlaskEm.AddPage("main.html",a=5,b=7)
-

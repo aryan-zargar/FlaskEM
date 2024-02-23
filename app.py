02e5df5aedc5d2afe560592f39a59c2cad2c67ec
aryan_zargar <aryan.zargar90@gmail.com>
2024-02-23 19:41:46 +0330

    V1.3

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import commands.commands as FlaskEm
 from flask import Flask
 app = Flask(__name__)
 db = FlaskEm.ConfigDataBase(app,"sqlite:///YOUR_DATABASE_NAME.db")
-
+s
 @app.route("/")
 def mainpage():
     return FlaskEm.addPage("main.html",a="test value")
--- commands/commands.py
+++ commands/commands.py
@@ -1,10 +1,12 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import *
-def addPage(FileName,**kwargs):
+def addPageHtml(FileName,**kwargs):
     theReturnDict = {}
     for key,value in kwargs.items():
         theReturnDict[key] = value
     return render_template(FileName,values=theReturnDict)
+def addPage(data):
+    return (data)
 def ConfigDataBase(app,location):
     app.config['SQLALCHEMY_DATABASE_URI'] = location
     app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
